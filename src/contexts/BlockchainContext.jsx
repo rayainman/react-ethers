71bd27e940cab280f0d839b0270c0f7a1991a6c5
Raymond He <sihruei.he@gmail>
2022-06-06 11:26:22 +0800

    作業

--- src/contexts/BlockchainContext.jsx
+++ src/contexts/BlockchainContext.jsx
@@ -3,11 +3,12 @@ import { ethers } from "ethers";
 
 export const BlockchainContext = React.createContext({
   currentAccount: null,
-  provider: null
+  provider: null,
+  chain: null
 });
 
 const BlockchainContextProvider = ({ children }) => {
-  const [currentAccount, setCurrentAccount] = React.useState(null);
+  const [currentAccount, updateCurrentAccounts] = React.useState(null);
   const [provider, setProvider] = React.useState(null);
 
   React.useEffect(() => {
@@ -19,6 +20,43 @@ const BlockchainContextProvider = ({ children }) => {
      * 加分項目2: 使用 window.ethereum 偵測目前的鏈是否為 Rinkeby，如果不是，則透過 window.ethereum 跳出換鏈提示
      * 提示: Rinkeby chain ID 為 0x4
      */
+    
+
+
+    window.ethereum?.request({ method: 'eth_requestAccounts' }).then(updateCurrentAccounts);
+    window.ethereum?.on("accountsChanged", updateCurrentAccounts);
+
+
+    const hintChangeCurrentChain = (chainId) => {
+      if (!chainId ||  chainId != "0x4") {
+          window.ethereum?.request({
+            method: "wallet_switchEthereumChain",
+            params: [{ chainId: "0x4" }],
+          })
+          .catch((error) => {
+            if (error.code == -32002) {
+              window.alert("請確認metamask鏈是否為Rinkeby");
+            }
+          });
+      }
+    }
+    window.ethereum?.on("chainChanged", hintChangeCurrentChain);
+
+
+    // ethereum.on('chainChanged', (chainId) => {
+    //   if (chainId != "0x4") {
+    //     // alert("change youre chain");
+    //     window.ethereum.request({ method: 'wallet_switchEthereumChain ' }).then(updateCurrentAccounts);
+    //   }
+      // Handle the new chain.
+      // Correctly handling chain changes can be complicated.
+      // We recommend reloading the page unless you have good reason not to.
+    //   window.location.reload();
+    // });
+    return () => {
+      window.ethereum?.removeListener('accountsChanged', updateCurrentAccounts);
+      window.ethereum?.removeListener("chainChanged", hintChangeCurrentChain);
+    }
   }, []);
 
   React.useEffect(() => {
@@ -27,6 +65,8 @@ const BlockchainContextProvider = ({ children }) => {
      * 透過 Web3Provider 將 window.ethereum 做為參數建立一個新的 web3 provider
      * 並將這個新的 web3 provider 設定成 provider 的 state
      */
+    const provider = new ethers.providers.Web3Provider(window.ethereum);  
+    setProvider(provider);
   }, []);
 
   return (
